29a5f8a69e05b44633d118d6c2a7b7bcf52f5c03
Ricard Garcia <imricardgarcia@gmail.com>
2021-11-19 17:06:36 +0100

    feat: added API limit protection

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
+
 import { SERVER } from "./constants/routes";
 
 import { timeRouter } from "./routes/time-routes";
@@ -13,5 +15,13 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
+app.use(
+  rateLimit({
+    windowMs: 24 * 60 * 60 * 1000, // 1 day
+    max: 500,
+    message:
+      "Reached the limit of queries to MVST. Time Coding Challenge per day.",
+  }),
+);
 
 app.use(`${SERVER.MAIN}`, timeRouter);
