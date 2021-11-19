9ed2f3b13296185b377a2ae0813445fd5efe24de
Ricard Garcia <imricardgarcia@gmail.com>
2021-11-19 10:56:07 +0100

    fix: solved missing value bug on localstorage

--- packages/client/src/utils/localStorage.ts
+++ packages/client/src/utils/localStorage.ts
@@ -2,7 +2,12 @@ export function setLocalStorage(key: string, value: string): void {
   localStorage.setItem(key, value);
 }
 
-export function getLocalStorage(key: string): string {
-  const localStorageValue = JSON.parse(localStorage.getItem(key) || "{}");
-  return localStorageValue;
+export function getLocalStorage(key: string): any {
+  const localStorageValue = localStorage.getItem(key);
+  if (typeof localStorageValue === "string") {
+    const returnedValue = JSON.parse(localStorageValue);
+    return returnedValue;
+  } else {
+    setLocalStorage(key, "true");
+  }
 }
